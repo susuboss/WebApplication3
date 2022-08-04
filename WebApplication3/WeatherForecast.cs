aff7301d4cb9b9da1b6f7f9329505d8c43fe14d0
susuboss <privatesusu@gmail.com>
2022-08-04 15:41:26 +0700

    sua file

--- WebApplication3/WeatherForecast.cs
+++ WebApplication3/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace WebApplication3
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.56556);
 
         public string? Summary { get; set; }
     }
