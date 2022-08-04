931f80ce49e4831dc9a488b72bb00e5eb769fdd3
susuboss <48903488+susuboss@users.noreply.github.com>
2022-08-04 15:51:39 +0700

    them so 5

--- WebApplication3/Controllers/WeatherForecastController.cs
+++ WebApplication3/Controllers/WeatherForecastController.cs
@@ -24,7 +24,7 @@ namespace WebApplication3.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
+                TemperatureC = Random.Shared.Next(-20, 555),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
